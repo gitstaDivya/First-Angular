6739b6b4d20b223327da6ba73781fa51e6d4aeb5
Divya Jain <19ucs216@lnmiit.ac.in>
2024-03-10 21:35:07 +0530

    Todo on Local Storage

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -11,32 +11,51 @@ inputTodo: string="";
   constructor() { }
 
   ngOnInit(): void {
-    this.todosArray =[
-      // {
-      //   content:'',
-      //   completed:false
-      // }
-    ]
+    // this.todosArray =[
+    //   // {
+    //   //   content:'',
+    //   //   completed:false
+    //   // }
+    // ]
+    const storedTodos = localStorage.getItem('todos');
+    this.todosArray = storedTodos ? JSON.parse(storedTodos) : [];
   }
 
   toggleDone(id:number){
-    this.todosArray.map((to,i)=>{
-      if(i==id)
-        to.completed = !to.completed;
-      return to;
-    })
+    // this.todosArray.map((to,i)=>{
+    //   if(i==id)
+    //     to.completed = !to.completed;
+    //   return to;
+    // })
+    this.todosArray[id].completed = !this.todosArray[id].completed;
+    this.updateLocalStorage();
   }
 
   deleteTodo(id:number){
-    this.todosArray = this.todosArray.filter((to,i)=> i!=id);
+    // this.todosArray = this.todosArray.filter((to,i)=> i!=id);
+    this.todosArray = this.todosArray.filter((todo, index) => index !== id);
+    this.updateLocalStorage();
   }
 
   addTodo(){
+    // this.todosArray.push({
+    //   content: this.inputTodo,
+    //   completed: false
+    // });
+    // this.inputTodo="";
+    if (this.inputTodo.trim() === '') {
+      return; // Don't add empty todos
+    }
+
     this.todosArray.push({
       content: this.inputTodo,
       completed: false
     });
-    this.inputTodo="";
-  }
 
+    this.inputTodo = '';
+    this.updateLocalStorage();
+  }
+  private updateLocalStorage(): void {
+    localStorage.setItem('todos', JSON.stringify(this.todosArray));
+  }
 }
